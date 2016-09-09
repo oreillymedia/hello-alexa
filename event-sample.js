8024aa175e747b740c2aaca866de99a8201ff984
Brian Jepson <bjepson@gmail.com>
2016-09-09 17:42:26 -0400

    more docs; sample request

---
+++ event-sample.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "session": {
+    "sessionId": "SessionId.46928689-1959-4658-a831-900452d7dab7",
+    "application": {
+      "applicationId": "amzn1.ask.skill.91631641-6bb7-4766-91c9-daa653cdf539"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.AFP3ZWPOS2BGJR7OWJZ3DHPKMOMK7UQEXD2DFRZVCFU4KU6QVKVTNCWVSUSI72XDTMASEXJ25CYDMILJ376VOVLJTO4JU5RRPP2YWU2DTZ7TC6M4VAGM4JUT4SPTBZO6JUF62HGUIFTXDP7QHQ7XOVTB3KQRSD353J5RA5W7AIV4TMMSCIY33LESLOC3GFXAFQFPWL4AXNEOGFI"
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.f70bdf4d-beeb-4dff-84aa-8c14004b2672",
+    "locale": "en-US",
+    "timestamp": "2016-09-09T20:14:46Z",
+    "intent": {
+      "name": "GetTideIntent",
+      "slots": {
+        "City": {
+          "name": "City",
+          "value": "Freeport me"
+        }
+      }
+    }
+  },
+  "version": "1.0"
+}
